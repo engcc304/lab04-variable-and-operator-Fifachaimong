eec1e90702c6f227109395df2c0036cd6c720a2c
your name <fifasisa36@gmail.com>
2023-08-11 16:00:49 +0700

    update hw

--- Homework/HW04/hw04-1-variable.cpp
+++ Homework/HW04/hw04-1-variable.cpp
@@ -4,9 +4,14 @@
     Output:
         10 20.0 Haha a
 */
+#include <stdio.h>
 
+int main(){
 int a = 10 ;
-float b = 20 ;
+float b = 20.0 ;
 char c[] = "Haha" ;
 char d = 'a' ;
-printf( "_ _ _ _", _, _, _, _) ; //TODO: #10 Display variable a, b, c and d.
+printf( "_ _ _ _\n") ; 
+scanf("%d",a ,b ,c ,d);
+printf("10 20.0 Haha a\n", a ,b ,c ,d);
+}//end main function
--- Lab/Lab01-Basic-Display-Input-Output.cpp
+++ Lab/Lab01-Basic-Display-Input-Output.cpp
@@ -1,14 +1,13 @@
-#include <stduio.h>
+#include <stdio.h>
 
 int main() {
     char Name[50] ;
     int  Age = 0 ;
-    printf( "Enter your name: " ) 
+    printf( "Enter your name: " ) ;
     scanf( "%s", Name ) ;
     printf( "Enter your age: " ) ;
     scanf( "%d", &Age ) ;
-    print( "- - - - - -\n" ) ;
-    printf( "Hello %s \n", ___ ) ; //TODO: #14 Say hello to user.
-    printf( "Age = %d\n", ___ ) ; //TODO: #15 display user their age number plus 1.
-    printf()
+    printf( "- - - - - -\n" ) ;
+    printf( "Hello %s\n", Name ) ; //TODO: #14 Say hello to user.
+    printf( "Age = %d\n", Age ) ; //TODO: #15 display user their age number plus 1.
 }//end main function
